b260804d3834382e07368f599dc2bce1755ce61c
d <nachmad@wwu.edu>
2018-04-12 20:43:13 -0700

    loading spinner module installed

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router();
+var sleep = require('system-sleep');
 
 
-router.post('/createExperiment',(req,res) => {   
+
+router.post('/createExperiment',  (req,res) => {   
     console.log(req.body);
     //pid = req.body[0].pid;
     info = [{pid: '1ake', id : '12'}];
+    sleep(10000);
+   
 	res.send(JSON.stringify(info));
 });
 
