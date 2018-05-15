08af5dfc2129121e325aefb71719206bef07cb9a
David Nachmanson <nachmad@procavx.proj.cs.wwu.edu>
2018-05-14 22:17:01 -0700

    select set of residues properly

--- src/app/experiment-results/experiment-results.component.ts
+++ src/app/experiment-results/experiment-results.component.ts
@@ -17,59 +17,60 @@ export class ExperimentResultsComponent   {
     color;
     viewer;
     parent;
+    structure;
+    orig_structure;
     pocketNums;
     pocketNumber:number;
 
 
   constructor(private userInputService : UserInputService,
 	      private globaldata : GlobalData) {
- 	this.pocketNums = Array.from(this.globaldata.getMap().keys());
-	this.experimentId = this.globaldata.getExperimentId();
-	console.log(this.pocketNums);
   }
 
 
   ngOnInit() {
-    console.log(this.experimentId);
-    this.pid = this.globaldata.getPid();
-    console.log(this.pid)
-	  this.parent = document.getElementById('viewer');
-	  this.color = pv.color;
-	  console.log(this.parent);
-  }
+	this.pocketNums = Array.from(this.globaldata.getMap().keys());
+	this.experimentId = this.globaldata.getExperimentId();
+	console.log(this.experimentId);
+	this.pid = this.globaldata.getPid();
+	console.log(this.pid)
+	this.parent = document.getElementById('viewer');
+	this.color = pv.color;
+
     
-  ngAfterContentInit() {
 	var options = {
           width: 600,
           height: 600,
           antialias: true,
           quality : 'high'
          };
+ 	
+	this.viewer = pv.Viewer(this.parent, options);
+ 	this.color = pv.color;
+        var url = "http://www.rcsb.org/pdb/files/";
+        url = url + this.pid + ".pdb";
+        pv.io.fetchPdb(url, (structure) => {
+	    this.orig_structure = structure;
+            this.viewer.cartoon('protein', structure);
+            this.viewer.autoZoom();
+            this.viewer.centerOn(structure);
 
-        console.log(this.parent);
-
- 	this.viewer = pv.Viewer(this.parent, options);
-	var viewer_ = this.viewer;
- 	  this.color = pv.color;
-	  console.log(this.viewer);
-          var url = "http://www.rcsb.org/pdb/files/";
-          url = url + this.pid + ".pdb";
-          pv.io.fetchPdb(url, function(structure) {
-           viewer_.cartoon('protein', structure);
-            viewer_.autoZoom();
-            viewer_.centerOn(structure);
-
-	 });
+	});
+	
 }
 
+
  colorPocketResidues(pocketNum : number){
+    this.viewer.clear();
+    this.structure = this.orig_structure;
+    this.viewer.cartoon('protein',this.structure);
     var residues = this.globaldata.getPocketResidues(pocketNum);
-    var color_ = this.color;
-    this.viewer.forEach(function(go) {
-	console.log(go);
-	go.colorBy(color_.uniform('red'), go.select({rindices : residues}));
+    this.viewer.forEach( (structure) => {
+	structure.colorBy(this.color.uniform('red'), structure.select({rindices : residues}));
     });
     this.viewer.requestRedraw();
 }
 
 }
+
+
