9e37764974310d899f15f06ccaa942f36baa4fa7
kvams <kvams@wwu.edu>
2018-05-15 16:35:08 -0700

    JSON object with every valid PID is printed to console on website startup-next steps convert to HashMap

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -4,6 +4,8 @@ import { HttpClient , HttpErrorResponse} from '@angular/common/http';
 import {UserInputService} from '../userinput.service';
 import {ServerService} from '../server.service';
 import {GlobalData} from '../globaldata.service';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-user-input',
@@ -17,6 +19,23 @@ export class UserInputComponent implements OnInit {
 	      private serverService : ServerService,
 	      private globalData : GlobalData) {
       console.log('user-input.component.ts constructor initialized.');
+      
+      var url = 'https://www.rcsb.org';
+      var opts = '/pdb/rest/getCurrent';
+      let parseString = require('xml2js').parseString;
+      var xml;
+      this.http.get(url+opts, {responseType:'text'})
+	  .map(response => {
+	      let data;
+	      parseString(response, function(err, result) {
+		  console.dir(result); // print JSON object
+		  data = result
+	      });
+	      return data;
+	  })
+	  .subscribe(data => {
+	      console.log(data); // print again...
+	  }); 
   }
     
     ngOnInit() {
@@ -29,46 +48,12 @@ export class UserInputComponent implements OnInit {
 	if (form.valid){// && checkPidValidity(form.value.pid)) {
 	    this.userInputService.pidEntered.emit(form.value.pid);
 	    this.globalData.setPid(form.value.pid);
-	}// else {
-	// validation failed
-	//}
-	//need make sure pid exists
-	// if(form.valid){
-	//   this.serverService.checkIfPidExists("1ake").subscribe(
-	//     (error : HttpErrorResponse) => {console.log(error.status);
-	//     console.log("something");})
-	//   }
-    }
-/*
-    checkPidValidity(pid : string) {
-	console.log('checking pid validity...');
-	var url = 'https://www.rcsb.org/pdb/rest/idStatus?structureId=' + pid + '';
-	var body = '';
-	this.http.post(url, body, {
-	    headers: new HttpHeaders()
-	        .set('Content-Type', 'text/xml')
-	        .append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
-	        .append('Access-Control-Allow-Origin', '*')
-	        .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method")
-	    , responseType:'text'
-	}).subscribe(data => {
-	    console.log(data);
-	},
-	(err : HttpErrorResponse) => {
-	    if (err.error instanceOf Error) {
-		// client side or network error
-		console.log('Error: ', err.error.message);
-	    } else {
-		// backend returned unsuccessful response code
-		console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
-	    }
 	}
-      );
-    }*/
-      //this.serverService.getPidStatus(form.value.pid).subscribe(
-      // data => { this.foods = data },
+    }
+    
     submitExperimentId(form : NgForm){
 	if(form.valid) this.userInputService.experimentIdEntered.emit(form.value.experimentId);
 	this.globalData.setExperimentId(form.value.experimentId);
     }
+
 }
