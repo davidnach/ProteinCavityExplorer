69f38f8c94c0f92a2da5eaff7d2adc106039da23
davidnach <davidnachson@gmail.com>
2019-08-01 19:51:22 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,55 +13,57 @@ import {GlobalData} from './globaldata.service';
 
 export class AppComponent {
      public loading = false;  
-constructor(private serverService: ServerService,
-  private router: Router,private userInputService: UserInputService,
-private globaldata : GlobalData ){
-
-    this.userInputService.pidEntered.subscribe(
-       (pid : string) => {
-         this.submitPidToServer(pid);
-          });
-
-   this.userInputService.experimentIdEntered.subscribe(
-	(expId : string) => {
-	  this.submitExperimentId(expId);
-	});
-   }
+     constructor(private serverService: ServerService,
+     private router: Router,private userInputService: UserInputService,
+     private globaldata : GlobalData ){
+     
+     this.userInputService.pidEntered.subscribe(
+        (pid : string) => {
+            this.submitPidToServer(pid);
+     });
+     this.userInputService.experimentIdEntered.subscribe(
+	      (expId : string) => {
+	          this.submitExperimentId(expId);
+	   });
+ }
 
   submitPidToServer(pid : string){
-   this.loading = true;
-   this.serverService.createExperiment({pid: pid})
-    .subscribe((response) => {
-      console.log(response['expId']);
-      this.globaldata.setExperimentId(response['expId']);
-      this.globaldata.setPockets(response['pockets']);
-this.loading = false;
-console.log(this.loading);	
-      this.router.navigate(['experiment']);},
-    (error) => {this.loading = false;
-console.log(error);});
-
-
+      this.loading = true;
+      this.serverService.createExperiment({pid: pid})
+          .subscribe((response) => {
+              console.log(response['expId']);
+              this.globaldata.setExperimentId(response['expId']);
+              this.globaldata.setPockets(response['pockets']);
+              this.loading = false;
+              console.log(this.loading);	
+              this.router.navigate(['experiment']);},
+              (error) => {
+                  this.loading = false;
+                  console.log(error);
+               }
+        );
   }
   
   submitExperimentId(experimentId : string){
-     this.loading = true;
-    this.serverService.retrieveExperiment({expId : experimentId})
-    .subscribe((response) => {
-    console.log(response);
-    if(response === 'invalid') {
-      this.globaldata.setExperimentId('invalid');
-    } else {
-      this.globaldata.setPid(response['pid']);
-      this.globaldata.setExperimentId(response['expId']);
-      this.globaldata.setPockets(response['pockets']);
-         this.loading = false;
-      this.router.navigate(['experiment']);
-    }	
-
-    },	
-    (error) => {console.log(error)
-       this.loading =false;});
-  }
+      this.loading = true;
+      this.serverService.retrieveExperiment({expId : experimentId})
+      .subscribe((response) => {
+          console.log(response);
+          if(response === 'invalid') {
+              this.globaldata.setExperimentId('invalid');
+          } else {
+              this.globaldata.setPid(response['pid']);
+              this.globaldata.setExperimentId(response['expId']);
+              this.globaldata.setPockets(response['pockets']);
+              this.loading = false;
+              this.router.navigate(['experiment']);
+          }	
+      },	
+      (error) => {
+          console.log(error)
+          this.loading = false;
+      }
+   );
+ }
 
 }
